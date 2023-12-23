805ce1f57c27596c9569db3c9cdf0bbc096244a6
Taimoor Hussain <taimoorhussain985@gmail.com>
2023-12-24 00:04:20 +0500

    Custom Data Fetching Hook

--- src/react-query/TodoList.tsx
+++ src/react-query/TodoList.tsx
@@ -1,26 +1,10 @@
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
-
-interface Todo {
-  id: number;
-  title: string;
-  userId: number;
-  completed: boolean;
-}
+import useTodos from "./hooks/useTodos";
 
 const TodoList = () => {
-  const fetchTodos = () =>
-    axios
-      .get<Todo[]>("https://jsonplaceholder.typicode.com/todos")
-      .then((res) => res.data);
-
-  const { data: todos, error, isLoading } = useQuery<Todo[], Error>({
-    queryKey: ["Todos"],
-    queryFn: fetchTodos,
-  });
+  const { data: todos, error, isLoading } = useTodos();
 
   if (error) return <b>{error.message}</b>;
-  if(isLoading) return <b>Loading... </b>
+  if (isLoading) return <b>Loading... </b>;
 
   return (
     <ul className="list-group">
