42d6f4d94eeecd26a9baf1ac9007e27c3a3f772a
Taimoor Hussain <taimoorhussain985@gmail.com>
2023-12-24 00:22:49 +0500

    Customizing Query Settings

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,20 @@ import "./index.css";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient(
+//   {
+//   defaultOptions: {
+//     queries: {
+//       retry: 3,
+//       cacheTime: 300_000, // 5mint
+//       staleTime: 10 * 10000, //fresh data
+//       refetchOnMount: false,
+//       refetchOnReconnect: false,
+//       refetchOnWindowFocus: false
+//     },
+//   },
+// }
+);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
