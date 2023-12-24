0826d173940cd78489162fb2b11a25434e3b9127
Taimoor Hussain <taimoorhussain985@gmail.com>
2023-12-24 22:10:51 +0500

    Paginated Queries

--- src/react-query/PostList.tsx
+++ src/react-query/PostList.tsx
@@ -2,24 +2,22 @@ import { useState } from "react";
 import usePosts from "./hooks/usePosts";
 
 const PostList = () => {
-  const [userId, setUserId] = useState<number>();
-  const { data: posts, error, isLoading } = usePosts(userId);
+  const pageSize = 10;
+  const [page, setPage] = useState(1);
+  const {
+    data: posts,
+    error,
+    isLoading,
+  } = usePosts({
+    page,
+    pageSize,
+  });
 
   if (error) return <p>{error.message}</p>;
   if (isLoading) return <p>Loading....</p>;
 
   return (
     <>
-      <select
-        onChange={(event) => setUserId(parseInt(event.target.value))}
-        value={userId}
-        className="form-select mb-3"
-      >
-        <option value=""></option>
-        <option value="1">User1</option>
-        <option value="2">User2</option>
-        <option value="3">User3</option>
-      </select>
       <ul className="list-group">
         {posts?.map((post) => (
           <li key={post.id} className="list-group-item">
@@ -27,6 +25,22 @@ const PostList = () => {
           </li>
         ))}
       </ul>
+
+      <button
+        disabled={page === 1}
+        onClick={() => setPage(page - 1)}
+        className="btn btn-primary ms-1 my-3"
+      >
+        Previous
+      </button>
+
+      <button
+        disabled={page === pageSize}
+        onClick={() => setPage(page + 1)}
+        className="btn btn-primary ms-1 my-3"
+      >
+        Next
+      </button>
     </>
   );
 };
--- src/react-query/hooks/usePosts.tsx
+++ src/react-query/hooks/usePosts.tsx
@@ -7,19 +7,25 @@ interface Post {
   body: string;
   userId: number;
 }
-const usePosts = (userId: number | undefined) => {
+interface PostQuery {
+  page: number;
+  pageSize: number;
+}
+const usePosts = (query: PostQuery) => {
   const fetchPosts = () =>
     axios
       .get<Post[]>("https://jsonplaceholder.typicode.com/posts", {
         params: {
-          userId,
+          _start: (query.page - 1) * query.pageSize,
+          _limit: query.pageSize,
         },
       })
       .then((res) => res.data);
   return useQuery<Post[], Error>({
-    queryKey: userId ? ["users", userId, "posts"] : ["posts"],
+    queryKey: ["posts", query],
     queryFn: fetchPosts,
     staleTime: 1 * 60 * 1000,
+    keepPreviousData: true,
   });
 };
 
