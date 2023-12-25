b221e364bfeca55cc0469bae942af079fd4ea7ce
Taimoor Hussain <taimoorhussain985@gmail.com>
2023-12-25 21:56:08 +0500

    Reusable HTTP Service

--- src/react-query/hooks/useAddTodos.ts
+++ src/react-query/hooks/useAddTodos.ts
@@ -1,10 +1,6 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { Todo } from "./useTodos";
-
+import todoService, { Todo } from "../services/todoService";
 import { CACHE_KEY_TODOS } from "../constants";
-import APIClient from "../services/apiClient";
-
-const apiClient = new APIClient<Todo>("/todos");
 interface AddTodoContext {
   previousTodos: Todo[];
 }
@@ -12,11 +8,11 @@ interface AddTodoContext {
 const useAddTodos = (onAdd: () => void) => {
   const queryClient = useQueryClient();
   return useMutation<Todo, Error, Todo, AddTodoContext>({
-    mutationFn: apiClient.post,
+    mutationFn: todoService.post,
     // onMutate execute before mutation
     onMutate: (newTodo: Todo) => {
       const previousTodos =
-      queryClient.getQueryData<Todo[]>(CACHE_KEY_TODOS) || [];
+        queryClient.getQueryData<Todo[]>(CACHE_KEY_TODOS) || [];
       queryClient.setQueryData<Todo[]>(CACHE_KEY_TODOS, (todos = []) => [
         newTodo,
         ...todos,
--- src/react-query/hooks/useTodos.tsx
+++ src/react-query/hooks/useTodos.tsx
@@ -1,19 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
-import APIClient from "../services/apiClient";
+import todoService, { Todo } from "../services/todoService";
 
-export interface Todo {
-  id: number;
-  title: string;
-  userId: number;
-  completed: boolean;
-}
 
-const apiClient = new APIClient<Todo>("/todos");
+
 
 const useTodos = () => {
   return useQuery<Todo[], Error>({
     queryKey: ["todos"],
-    queryFn: apiClient.getAll,
+    queryFn: todoService.getAll,
     staleTime: 10 * 1000, //10 sec (  In React Query, "stale time" refers to the amount of time after which cached data is considered outdated and potentially needs to be refreshed.)
   });
 };
