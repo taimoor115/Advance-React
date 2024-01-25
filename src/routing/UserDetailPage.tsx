4cec9c62925d74106a4733ca12f3bb8a75645ca2
Taimoor Hussain <taimoorhussain985@gmail.com>
2024-01-25 23:15:50 +0500

    useParams, useLocation, useSearchParams

--- src/routing/UserDetailPage.tsx
+++ src/routing/UserDetailPage.tsx
@@ -1,4 +1,15 @@
+import { useLocation, useParams } from "react-router";
+import { useSearchParams } from "react-router-dom";
+
 const UserDetailPage = () => {
+  const params = useParams();
+  console.log(params);
+  const location = useLocation();
+  console.log(location);
+
+  const [search, setSearchParams] = useSearchParams();
+  console.log(search.get('name'));
+
   return <p>User</p>;
 };
 
