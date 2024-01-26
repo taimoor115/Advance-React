5d2daa16cee750c2a00e2ef6f334647ba956c554
Taimoor Hussain <taimoorhussain985@gmail.com>
2024-01-26 12:51:19 +0500

    Nested Routes

--- src/routing/Routing.tsx
+++ src/routing/Routing.tsx
@@ -1,14 +1,20 @@
 import { createBrowserRouter } from "react-router-dom";
+import ContactPage from "./ContactPage";
 import HomePage from "./HomePage";
 import UserListPage from "./UserListPage";
-import ContactPage from "./ContactPage";
-import UserDetailPage from "./UserDetailPage";
+
+import Layout from "./Layout";
 
 const router = createBrowserRouter([
-  { path: "/", element: <HomePage /> },
-  { path: "/users", element: <UserListPage /> },
-  { path: "/contact", element: <ContactPage /> },
-  { path: "/users/:id", element: <UserDetailPage /> },
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "users", element: <UserListPage /> },
+      { path: "contact", element: <ContactPage /> },
+    ],
+  },
 ]);
 
 export default router;
